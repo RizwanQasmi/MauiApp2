ee487fb380364ba5fb1322209c8e93f3a6c6a284
Rizwan Khan <qasmi.rizwan@gmail.com>
2024-10-08 18:45:13 +0530

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ZXing.Net.Maui;
+using ZXing.Net.Maui.Controls;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //cameraBarcodeReaderView.BarcodesDetected += (s, e) =>
+            //{
+            //    foreach (var barcode in e.Value)
+            //    {
+            //        // Display the scanned QR code result
+            //        MainThread.BeginInvokeOnMainThread(() =>
+            //        {
+            //            DisplayAlert("QR Code", $"Scanned: {barcode.Value}", "OK");
+            //        });
+            //    }
+            //};
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ MauiApp2/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using ZXing.Net.Maui;
+using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
+
+
+namespace MauiApp2
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                //.UseZXingNetMaui() // This initializes ZXing
+                //.UseZXingNetMauiControls()
+                .UseBarcodeReader()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
